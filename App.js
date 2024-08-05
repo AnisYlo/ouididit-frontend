fad7e5186e14284997d91cc74d7fd7ec46a7a935
Anis YALAOUI <anisyalaoui68@gmail.com>
2024-08-05 17:16:43 +0200

    commit pour le 1er merge

--- App.js
+++ App.js
@@ -1,31 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { useGoogleLogin } from '@react-oauth/google';
-
-const login = useGoogleLogin({
-  onSuccess: codeResponse => console.log(codeResponse),
-  flow: 'auth-code',
-});
-
-
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+// import { GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/google';
+import SignIn from './screens/SignIn';
 export default function App() {
   return (
-    <GoogleOAuthProvider clientId="<your_client_id>">...
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <MyCustomButton onClick={() => login()}>Sign in with Google 🚀</MyCustomButton>;
-      <StatusBar style="auto" />
-    </View>
-    </GoogleOAuthProvider>
+    
+     
+        <SignIn></SignIn>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,88 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
-import jwtDecode from 'jwt-decode';
+import React from 'react';
+// import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity,Image } from 'react-native';
+import RedButton from '../components/redButton';
+// import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+// import jwtDecode from 'jwt-decode';
 
-const clientId = '492308766796-4rukpcc44v9mhjrtk98ibj9eoan212qa.apps.googleusercontent.com';
+const clientId = '893917699125-l9kugcm1qd3rkelgn479pldmipn58706.apps.googleusercontent.com';
 
 function SignIn() {
-  const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    GoogleSignin.configure({
-      webClientId: clientId,
-    });
-  }, []);
+  return(
 
-  const handleLogin = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      const decodedUser = jwtDecode(userInfo.idToken);
-      setUser(decodedUser);
-    } catch (error) {
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        console.log('User cancelled the login flow');
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        console.log('Signin in progress');
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        console.log('Play services not available or outdated');
-      } else {
-        console.log('Some other error happened');
-      }
-    }
-  };
+ <View style={styles.container}>
+    <Image source={require('../assets/logo.png')} style={styles.logo}>
+    </Image>
+    <TextInput>INPUT</TextInput>
+    <TextInput>INPUT</TextInput>
+    <Text> Choose another account </Text>
+   <RedButton buttonText='Sign In'>
 
-  return (
-    <View style={styles.container}>
-      {user ? (
-        <View style={styles.content}>
-          <Text style={styles.title}>Welcome {user.name}!</Text>
-          <View style={styles.divider}></View>
-          <Text>Email: {user.email}</Text>
-        </View>
-      ) : (
-        <View style={styles.content}>
-          <Text style={styles.title}>Welcome</Text>
-          <Text style={styles.subtitle}>Log in to your Google account to continue</Text>
-          <View style={styles.divider}></View>
-          <GoogleSigninButton
-            style={{ width: 192, height: 48 }}
-            size={GoogleSigninButton.Size.Wide}
-            color={GoogleSigninButton.Color.Dark}
-            onPress={handleLogin}
-          />
-        </View>
-      )}
+   </RedButton>
+    <View>
     </View>
-  );
+    <Text> Not register yet ? Create an account ! </Text>
+   <RedButton buttonText='Sign up'></RedButton>
+ </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  content: {
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  subtitle: {
-    fontSize: 18,
-    marginVertical: 10,
-  },
-  divider: {
-    height: 1,
-    width: '80%',
-    backgroundColor: '#ccc',
-    marginVertical: 20,
-  },
-});
+    container: {
+      flex: 1,
+      paddingTop: 50,
+      flexDirection: "column",
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      width: '100%',
+      height: '100%',
+      paddingBottom: 50,
+    },
+
+    logo: {
+      width: '100%',
+      height: '50%'
+    },
+    
+  });
+
 
 export default SignIn;
 
