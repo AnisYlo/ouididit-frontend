35b36544b0b20184d132fe58a081eec91c721512
Xavier N <x.naidet@gmail.com>
2024-08-05 17:55:10 +0200

    Correction Screens after Merges

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,18 +1,54 @@
-import { View, StyleSheet, Text, Button } from "react-native";
+import React from 'react';
+// import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity,Image } from 'react-native';
+import RedButton from '../components/redButton';
+// import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+// import jwtDecode from 'jwt-decode';
 
-export  default function SignInScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.navigate('SingUpScreen')} title="Sign Up" />
+const clientId = '893917699125-l9kugcm1qd3rkelgn479pldmipn58706.apps.googleusercontent.com';
+
+function SignInScreen({ navigation }) {
+
+  return(
+
+ <View style={styles.container}>
+    <Image source={require('../assets/logo.png')} style={styles.logo}>
+    </Image>
+    <TextInput>INPUT</TextInput>
+    <TextInput>INPUT</TextInput>
+    <Text> Choose another account </Text>
+   <RedButton buttonText='Sign In'>
+   <Button onPress={() => navigation.navigate('HomeScreen')} title="Sign Up" />
+
+   </RedButton>
+    <View>
     </View>
-  );
+    <Text> Not register yet ? Create an account ! </Text>
+   <RedButton buttonText='Sign up'></RedButton>
+ </View>
+  )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "green",
-      },
-});
+      flex: 1,
+      paddingTop: 50,
+      flexDirection: "column",
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      width: '100%',
+      height: '100%',
+      paddingBottom: 50,
+    },
+
+    logo: {
+      width: '100%',
+      height: '50%'
+    },
+    
+  });
+
+
+export default SignInScreen;
+
