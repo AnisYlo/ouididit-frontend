6e11d9f5c018cb2110d669cbe2923d280992de56
Anis YALAOUI <anisyalaoui68@gmail.com>
2024-08-06 19:56:20 +0200

    niquel

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,33 +1,47 @@
-import { View, StyleSheet, Button, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Button, TouchableOpacity, Text } from "react-native";
 import RedButton from "../components/redButton";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "../components/Header"
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
+    
+  <SafeAreaView style={styles.container}>
+    <View style={styles.header}>
+      <Header 
+        navigation={navigation}
+        title= "Home"
+        avatar={require("../assets/avatarDefault.png")}>
+      </Header>
+      <Text style={styles.title}>Home</Text>
+      </View>
+      
+        <View style={styles.activitiesCard}>
+        </View>
+        <View style={styles.chatCard}>
+        </View>
+            <RedButton
+              onPress={() => navigation.navigate('Create activity')}
+              title="Créer une activité"
+              buttonText='New Activity'
+            />
    
-      <View style={styles.activitiesCard}>
-      <TouchableOpacity></TouchableOpacity>
-      </View>
-      <View style={styles.chatCard}>
-      </View>
-      <RedButton
-        onPress={() => navigation.navigate('Create activity')}
-        title="Créer une activité"
-        buttonText='New Activity'
-      />
-    </View>
+    </SafeAreaView>
+   
   );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'center',
         justifyContent: "space-around",
-        alignItems: "center",
         backgroundColor: "white",
         paddingBottom: 50,
+        
       },
       activitiesCard: {
+      
         borderRadius: "20%",
         borderWidth: 2,
         borderColor: '#e74c3c',
@@ -36,6 +50,7 @@ const styles = StyleSheet.create({
         width: '95%'
       },
       chatCard: {
+      
         borderRadius: '20%',
         borderWidth: 2,
         borderColor: '#1F84D6',
@@ -43,5 +58,15 @@ const styles = StyleSheet.create({
         height: '40%',
         width: '95%'
 
+      },
+      title: {
+      width: '100%',
+      heigth: "25%",
+      fontFamily: 'ClashGrotesk-Bold',
+      fontSize: 24,
+      },
+      header: {
+        height: '10%',
+        width: "100%",
       }
 });
